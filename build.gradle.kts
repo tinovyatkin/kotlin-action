62c7e45c6baf5e11d7c372b56b5a2ab3a6c7ba9b
Konstantin Vyatkin <tino@vtkn.io>
2020-03-19 15:13:59 -0400

    Switch to Gradle/Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("js") version "1.3.70"
+}
+
+kotlin {
+    target {
+        useCommonJs()
+        nodejs()
+    }
+
+    sourceSets["main"].dependencies {
+        implementation(kotlin("stdlib-js"))
+
+        implementation(npm("@actions/core", "^1.2.3"))
+        implementation(npm("@actions/github", "^2.1.1"))
+    }
+}
+
+repositories {
+    mavenLocal()
+}
